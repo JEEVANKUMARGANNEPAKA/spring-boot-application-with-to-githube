551e343ec62a2c8d1d8bbc9c2e1b9eb02fe46ba0
Vinodkumar Reddy <jeevanbhavi440@gmail.com>
2024-05-30 19:47:09 +0530

    fourth-commit

--- src/main/java/com/jeevankumar/spring_boot_githube/SpringBootGithubeApplication.java
+++ src/main/java/com/jeevankumar/spring_boot_githube/SpringBootGithubeApplication.java
@@ -1,16 +1,22 @@
 package com.jeevankumar.spring_boot_githube;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping(value = "/api/v1")
 public class SpringBootGithubeApplication {
+	public static final Logger logger = LoggerFactory.getLogger(SpringBootGithubeApplication.class);
 
 	@GetMapping(value = "/hello")
 	public String hello(){
+		logger.info("This is log message");
 		return "Hello World....Application....";
 	}
 
